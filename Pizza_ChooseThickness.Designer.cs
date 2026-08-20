ed2662b5718b9fa68f64a465496387038dceaf71
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 10:25:15 +0300

    Design : Completing the interface for selecting the appropriate thickness

--- Pizza_ChooseThickness.Designer.cs
+++ Pizza_ChooseThickness.Designer.cs
@@ -29,7 +29,157 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_ChooseThickness));
+            this.lbl_Thick = new System.Windows.Forms.Label();
+            this.lbl_Price = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Thin = new System.Windows.Forms.Label();
+            this.btn_Thin = new System.Windows.Forms.Button();
+            this.btn_Thick = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbl_Thick
+            // 
+            this.lbl_Thick.AutoSize = true;
+            this.lbl_Thick.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Thick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Thick.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Thick.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Thick.Location = new System.Drawing.Point(208, 363);
+            this.lbl_Thick.Name = "lbl_Thick";
+            this.lbl_Thick.Size = new System.Drawing.Size(85, 34);
+            this.lbl_Thick.TabIndex = 25;
+            this.lbl_Thick.Text = "Thick";
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Price.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Price.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lbl_Price.Location = new System.Drawing.Point(710, 78);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(67, 60);
+            this.lbl_Price.TabIndex = 24;
+            this.lbl_Price.Text = "0$";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(682, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 60);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Price";
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.BackgroundImage = global::PizzaCraft.Properties.Resources.Exit;
+            this.btn_Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Exit.FlatAppearance.BorderSize = 0;
+            this.btn_Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Location = new System.Drawing.Point(39, 466);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(30, 30);
+            this.btn_Exit.TabIndex = 22;
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.BackgroundImage = global::PizzaCraft.Properties.Resources.Next;
+            this.btn_Next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Next.FlatAppearance.BorderSize = 0;
+            this.btn_Next.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Next.Location = new System.Drawing.Point(763, 465);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(40, 40);
+            this.btn_Next.TabIndex = 21;
+            this.btn_Next.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 60);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Select Suitable Thickness";
+            // 
+            // lbl_Thin
+            // 
+            this.lbl_Thin.AutoSize = true;
+            this.lbl_Thin.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Thin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Thin.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Thin.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Thin.Location = new System.Drawing.Point(506, 363);
+            this.lbl_Thin.Name = "lbl_Thin";
+            this.lbl_Thin.Size = new System.Drawing.Size(73, 34);
+            this.lbl_Thin.TabIndex = 26;
+            this.lbl_Thin.Text = "Thin";
+            // 
+            // btn_Thin
+            // 
+            this.btn_Thin.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Thin.BackgroundImage = global::PizzaCraft.Properties.Resources.Thin_Slice;
+            this.btn_Thin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Thin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Thin.FlatAppearance.BorderSize = 0;
+            this.btn_Thin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Thin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Thin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Thin.Location = new System.Drawing.Point(442, 155);
+            this.btn_Thin.Name = "btn_Thin";
+            this.btn_Thin.Size = new System.Drawing.Size(200, 200);
+            this.btn_Thin.TabIndex = 28;
+            this.btn_Thin.UseVisualStyleBackColor = false;
+            // 
+            // btn_Thick
+            // 
+            this.btn_Thick.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Thick.BackgroundImage = global::PizzaCraft.Properties.Resources.Think_Slice;
+            this.btn_Thick.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Thick.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Thick.FlatAppearance.BorderSize = 0;
+            this.btn_Thick.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Thick.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Thick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Thick.Location = new System.Drawing.Point(150, 155);
+            this.btn_Thick.Name = "btn_Thick";
+            this.btn_Thick.Size = new System.Drawing.Size(200, 200);
+            this.btn_Thick.TabIndex = 29;
+            this.btn_Thick.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(284, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "+1 $";
             // 
             // Pizza_ChooseThickness
             // 
@@ -38,14 +188,36 @@
             this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Thick);
+            this.Controls.Add(this.btn_Thin);
+            this.Controls.Add(this.lbl_Thin);
+            this.Controls.Add(this.lbl_Thick);
+            this.Controls.Add(this.lbl_Price);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pizza_ChooseThickness";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pizza Craft - Choose Appropriate Thickness";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Thick;
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Thin;
+        private System.Windows.Forms.Button btn_Thin;
+        private System.Windows.Forms.Button btn_Thick;
+        private System.Windows.Forms.Label label3;
     }
 }
