3e0295ff156068f9beb149098b3064c4327d8a7b
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 03:24:04 +0300

    Launch app with Pizza_Welcome form by default
    Modified startup logic to open Pizza_Welcome instead of Form1.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PizzaCraft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Pizza_Welcome());
         }
     }
 }
