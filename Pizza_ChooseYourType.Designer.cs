0c7f19393fe5ece6053c816dcdd16adc019e3a82
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 09:25:04 +0300

    Design : Selection interface from the ready-made menu

--- Pizza_ChooseYourType.Designer.cs
+++ Pizza_ChooseYourType.Designer.cs
@@ -29,7 +29,202 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_ChooseYourType));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Margherita = new System.Windows.Forms.Button();
+            this.btn_Chicken = new System.Windows.Forms.Button();
+            this.btn_FourCheese = new System.Windows.Forms.Button();
+            this.btn_Pepperoni = new System.Windows.Forms.Button();
+            this.btn_SupremeMeat = new System.Windows.Forms.Button();
+            this.btn_SupremeVeggie = new System.Windows.Forms.Button();
+            this.pic_Pizza = new System.Windows.Forms.PictureBox();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Price = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Pizza)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Type Of Order";
+            // 
+            // btn_Margherita
+            // 
+            this.btn_Margherita.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Margherita.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_Margherita.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Margherita.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Margherita.FlatAppearance.BorderSize = 0;
+            this.btn_Margherita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Margherita.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_Margherita.ForeColor = System.Drawing.Color.White;
+            this.btn_Margherita.Location = new System.Drawing.Point(39, 104);
+            this.btn_Margherita.Name = "btn_Margherita";
+            this.btn_Margherita.Size = new System.Drawing.Size(222, 41);
+            this.btn_Margherita.TabIndex = 0;
+            this.btn_Margherita.Text = "Margherita";
+            this.btn_Margherita.UseVisualStyleBackColor = false;
+            // 
+            // btn_Chicken
+            // 
+            this.btn_Chicken.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Chicken.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_Chicken.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Chicken.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Chicken.FlatAppearance.BorderSize = 0;
+            this.btn_Chicken.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Chicken.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_Chicken.ForeColor = System.Drawing.Color.White;
+            this.btn_Chicken.Location = new System.Drawing.Point(39, 159);
+            this.btn_Chicken.Name = "btn_Chicken";
+            this.btn_Chicken.Size = new System.Drawing.Size(222, 41);
+            this.btn_Chicken.TabIndex = 3;
+            this.btn_Chicken.Text = "Chicken";
+            this.btn_Chicken.UseVisualStyleBackColor = false;
+            // 
+            // btn_FourCheese
+            // 
+            this.btn_FourCheese.BackColor = System.Drawing.Color.Transparent;
+            this.btn_FourCheese.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_FourCheese.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_FourCheese.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_FourCheese.FlatAppearance.BorderSize = 0;
+            this.btn_FourCheese.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_FourCheese.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_FourCheese.ForeColor = System.Drawing.Color.White;
+            this.btn_FourCheese.Location = new System.Drawing.Point(39, 214);
+            this.btn_FourCheese.Name = "btn_FourCheese";
+            this.btn_FourCheese.Size = new System.Drawing.Size(222, 41);
+            this.btn_FourCheese.TabIndex = 4;
+            this.btn_FourCheese.Text = "Four Cheese";
+            this.btn_FourCheese.UseVisualStyleBackColor = false;
+            // 
+            // btn_Pepperoni
+            // 
+            this.btn_Pepperoni.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Pepperoni.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_Pepperoni.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Pepperoni.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Pepperoni.FlatAppearance.BorderSize = 0;
+            this.btn_Pepperoni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Pepperoni.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_Pepperoni.ForeColor = System.Drawing.Color.White;
+            this.btn_Pepperoni.Location = new System.Drawing.Point(39, 269);
+            this.btn_Pepperoni.Name = "btn_Pepperoni";
+            this.btn_Pepperoni.Size = new System.Drawing.Size(222, 41);
+            this.btn_Pepperoni.TabIndex = 5;
+            this.btn_Pepperoni.Text = "Pepperoni";
+            this.btn_Pepperoni.UseVisualStyleBackColor = false;
+            // 
+            // btn_SupremeMeat
+            // 
+            this.btn_SupremeMeat.BackColor = System.Drawing.Color.Transparent;
+            this.btn_SupremeMeat.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_SupremeMeat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_SupremeMeat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_SupremeMeat.FlatAppearance.BorderSize = 0;
+            this.btn_SupremeMeat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SupremeMeat.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_SupremeMeat.ForeColor = System.Drawing.Color.White;
+            this.btn_SupremeMeat.Location = new System.Drawing.Point(39, 324);
+            this.btn_SupremeMeat.Name = "btn_SupremeMeat";
+            this.btn_SupremeMeat.Size = new System.Drawing.Size(222, 41);
+            this.btn_SupremeMeat.TabIndex = 6;
+            this.btn_SupremeMeat.Text = "Supreme Meat";
+            this.btn_SupremeMeat.UseVisualStyleBackColor = false;
+            // 
+            // btn_SupremeVeggie
+            // 
+            this.btn_SupremeVeggie.BackColor = System.Drawing.Color.Transparent;
+            this.btn_SupremeVeggie.BackgroundImage = global::PizzaCraft.Properties.Resources.Button;
+            this.btn_SupremeVeggie.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_SupremeVeggie.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_SupremeVeggie.FlatAppearance.BorderSize = 0;
+            this.btn_SupremeVeggie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SupremeVeggie.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 12F);
+            this.btn_SupremeVeggie.ForeColor = System.Drawing.Color.White;
+            this.btn_SupremeVeggie.Location = new System.Drawing.Point(39, 379);
+            this.btn_SupremeVeggie.Name = "btn_SupremeVeggie";
+            this.btn_SupremeVeggie.Size = new System.Drawing.Size(222, 41);
+            this.btn_SupremeVeggie.TabIndex = 7;
+            this.btn_SupremeVeggie.Text = "Supreme Veggie";
+            this.btn_SupremeVeggie.UseVisualStyleBackColor = false;
+            // 
+            // pic_Pizza
+            // 
+            this.pic_Pizza.BackColor = System.Drawing.Color.Transparent;
+            this.pic_Pizza.BackgroundImage = global::PizzaCraft.Properties.Resources.MargheritaPizza;
+            this.pic_Pizza.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Pizza.Location = new System.Drawing.Point(314, 110);
+            this.pic_Pizza.Name = "pic_Pizza";
+            this.pic_Pizza.Size = new System.Drawing.Size(310, 310);
+            this.pic_Pizza.TabIndex = 8;
+            this.pic_Pizza.TabStop = false;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.BackgroundImage = global::PizzaCraft.Properties.Resources.Next;
+            this.btn_Next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Next.FlatAppearance.BorderSize = 0;
+            this.btn_Next.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Next.Location = new System.Drawing.Point(763, 465);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(40, 40);
+            this.btn_Next.TabIndex = 9;
+            this.btn_Next.UseVisualStyleBackColor = false;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.BackgroundImage = global::PizzaCraft.Properties.Resources.Exit;
+            this.btn_Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Exit.FlatAppearance.BorderSize = 0;
+            this.btn_Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Location = new System.Drawing.Point(39, 466);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(30, 30);
+            this.btn_Exit.TabIndex = 10;
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(667, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 60);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Price";
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Price.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Price.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lbl_Price.Location = new System.Drawing.Point(692, 166);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(67, 60);
+            this.lbl_Price.TabIndex = 12;
+            this.lbl_Price.Text = "0$";
             // 
             // Pizza_ChooseYourType
             // 
@@ -38,14 +233,41 @@
             this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Controls.Add(this.lbl_Price);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.pic_Pizza);
+            this.Controls.Add(this.btn_SupremeVeggie);
+            this.Controls.Add(this.btn_SupremeMeat);
+            this.Controls.Add(this.btn_Pepperoni);
+            this.Controls.Add(this.btn_FourCheese);
+            this.Controls.Add(this.btn_Chicken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Margherita);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pizza_ChooseYourType";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pizza Craft - Choose Your Type";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Pizza)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Margherita;
+        private System.Windows.Forms.Button btn_Chicken;
+        private System.Windows.Forms.Button btn_FourCheese;
+        private System.Windows.Forms.Button btn_Pepperoni;
+        private System.Windows.Forms.Button btn_SupremeMeat;
+        private System.Windows.Forms.Button btn_SupremeVeggie;
+        private System.Windows.Forms.PictureBox pic_Pizza;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Price;
     }
 }
