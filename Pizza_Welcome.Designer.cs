91cf4a661640e359d0a891809ee9dfb32e36be0d
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 06:55:12 +0300

    add welcome screen view on app launch

--- Pizza_Welcome.Designer.cs
+++ Pizza_Welcome.Designer.cs
@@ -29,7 +29,88 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_Welcome));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Info = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Aref_Menna", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(24, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 151);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pizza Craft";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Light", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(47, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 138);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Start.FlatAppearance.BorderSize = 0;
+            this.btn_Start.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Start.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Start.Font = new System.Drawing.Font("Aref_Menna", 15F);
+            this.btn_Start.ForeColor = System.Drawing.Color.White;
+            this.btn_Start.Location = new System.Drawing.Point(52, 327);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(123, 37);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            // 
+            // btn_Info
+            // 
+            this.btn_Info.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_Info.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Info.FlatAppearance.BorderSize = 0;
+            this.btn_Info.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Info.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Info.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Info.Font = new System.Drawing.Font("Aref_Menna", 15F);
+            this.btn_Info.ForeColor = System.Drawing.Color.White;
+            this.btn_Info.Location = new System.Drawing.Point(52, 370);
+            this.btn_Info.Name = "btn_Info";
+            this.btn_Info.Size = new System.Drawing.Size(123, 37);
+            this.btn_Info.TabIndex = 3;
+            this.btn_Info.Text = "Info";
+            this.btn_Info.UseVisualStyleBackColor = false;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Exit.FlatAppearance.BorderSize = 0;
+            this.btn_Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Font = new System.Drawing.Font("Aref_Menna", 15F);
+            this.btn_Exit.ForeColor = System.Drawing.Color.White;
+            this.btn_Exit.Location = new System.Drawing.Point(51, 413);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(123, 37);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = false;
             // 
             // Pizza_Welcome
             // 
@@ -38,16 +119,28 @@
             this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Info);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pizza_Welcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pizza Craft - Welcome";
             this.TransparencyKey = System.Drawing.Color.Maroon;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Info;
+        private System.Windows.Forms.Button btn_Exit;
     }
 }
 
