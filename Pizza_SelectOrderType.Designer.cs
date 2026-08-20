8f44afed6975f9153ad1027ddce297d1fcfff238
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 03:48:13 +0300

    Add Pizza_SelectOrderType form and resources
    Added Pizza_SelectOrderType form to PizzaCraft with designer and code-behind files. Updated Pizza_SelectOrderType.resx with embedded binary resource (image or icon).

---
+++ Pizza_SelectOrderType.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace PizzaCraft
+{
+    partial class Pizza_SelectOrderType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_SelectOrderType));
+            this.SuspendLayout();
+            // 
+            // Pizza_SelectOrderType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pizza_SelectOrderType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pizza Craft - Select Order Type";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
