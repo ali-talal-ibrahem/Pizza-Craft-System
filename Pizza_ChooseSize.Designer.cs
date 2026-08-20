6af5cf8607c2445eda6128a09bf9710d8cf53f88
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 03:53:43 +0300

    Add Pizza_ChooseSize form and resource to PizzaCraft
    Added designer and code-behind files for the Pizza_ChooseSize Windows Forms class, including form properties and initialization logic. Embedded a new binary resource (image or icon) in Pizza_ChooseSize.resx via base64-encoded data.

---
+++ Pizza_ChooseSize.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace PizzaCraft
+{
+    partial class Pizza_ChooseSize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_ChooseSize));
+            this.SuspendLayout();
+            // 
+            // Pizza_ChooseSize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pizza_ChooseSize";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pizza Craft - Choose Appropriate Size";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
