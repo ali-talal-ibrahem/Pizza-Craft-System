90f45c0650618a52d91b57807358d9131227d63f
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 03:52:12 +0300

    Add Pizza_ChooseToppings form and embed resource
    Added designer and code-behind files for the Pizza_ChooseToppings Windows Forms class in the PizzaCraft namespace. The designer sets form properties and resource cleanup; the code-behind initializes the form. Embedded a new binary resource (image or icon) as a base64 value in Pizza_ChooseToppings.resx without altering other resources.

---
+++ Pizza_ChooseToppings.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace PizzaCraft
+{
+    partial class Pizza_ChooseToppings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_ChooseToppings));
+            this.SuspendLayout();
+            // 
+            // Pizza_ChooseToppings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(814, 511);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pizza_ChooseToppings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pizza Craft - Choose Toppings";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
