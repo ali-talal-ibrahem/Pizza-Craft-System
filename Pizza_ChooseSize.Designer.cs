614732f668195393fd9135837b996e6f8185d225
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-20 10:06:51 +0300

    Design : Completion of the appropriate size selection interface

--- Pizza_ChooseSize.Designer.cs
+++ Pizza_ChooseSize.Designer.cs
@@ -29,7 +29,176 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pizza_ChooseSize));
+            this.lbl_Price = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Large = new System.Windows.Forms.Label();
+            this.lbl_Middle = new System.Windows.Forms.Label();
+            this.lbl_Small = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pic_Middle = new System.Windows.Forms.PictureBox();
+            this.pic_Large = new System.Windows.Forms.PictureBox();
+            this.pic_Small = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Middle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Large)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Small)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Price.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Price.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lbl_Price.Location = new System.Drawing.Point(710, 78);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(67, 60);
+            this.lbl_Price.TabIndex = 17;
+            this.lbl_Price.Text = "0$";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(682, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 60);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Price";
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.BackgroundImage = global::PizzaCraft.Properties.Resources.Exit;
+            this.btn_Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Exit.FlatAppearance.BorderSize = 0;
+            this.btn_Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Location = new System.Drawing.Point(39, 466);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(30, 30);
+            this.btn_Exit.TabIndex = 15;
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.BackgroundImage = global::PizzaCraft.Properties.Resources.Next;
+            this.btn_Next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Next.FlatAppearance.BorderSize = 0;
+            this.btn_Next.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Next.Location = new System.Drawing.Point(763, 465);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(40, 40);
+            this.btn_Next.TabIndex = 14;
+            this.btn_Next.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Anton", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 60);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Select Appropriate Size";
+            // 
+            // lbl_Large
+            // 
+            this.lbl_Large.AutoSize = true;
+            this.lbl_Large.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Large.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Large.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Large.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Large.Location = new System.Drawing.Point(35, 114);
+            this.lbl_Large.Name = "lbl_Large";
+            this.lbl_Large.Size = new System.Drawing.Size(90, 34);
+            this.lbl_Large.TabIndex = 18;
+            this.lbl_Large.Text = "Large";
+            // 
+            // lbl_Middle
+            // 
+            this.lbl_Middle.AutoSize = true;
+            this.lbl_Middle.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Middle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Middle.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Middle.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Middle.Location = new System.Drawing.Point(35, 236);
+            this.lbl_Middle.Name = "lbl_Middle";
+            this.lbl_Middle.Size = new System.Drawing.Size(104, 34);
+            this.lbl_Middle.TabIndex = 19;
+            this.lbl_Middle.Text = "Middle";
+            // 
+            // lbl_Small
+            // 
+            this.lbl_Small.AutoSize = true;
+            this.lbl_Small.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Small.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_Small.Font = new System.Drawing.Font("Teshrin AR+LT Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Small.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Small.Location = new System.Drawing.Point(35, 351);
+            this.lbl_Small.Name = "lbl_Small";
+            this.lbl_Small.Size = new System.Drawing.Size(92, 34);
+            this.lbl_Small.TabIndex = 20;
+            this.lbl_Small.Text = "Small";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(148, 121);
+            this.trackBar1.Maximum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 264);
+            this.trackBar1.TabIndex = 21;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 1;
+            // 
+            // pic_Middle
+            // 
+            this.pic_Middle.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pic_Middle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Middle.Location = new System.Drawing.Point(334, 124);
+            this.pic_Middle.Name = "pic_Middle";
+            this.pic_Middle.Size = new System.Drawing.Size(260, 260);
+            this.pic_Middle.TabIndex = 22;
+            this.pic_Middle.TabStop = false;
+            // 
+            // pic_Large
+            // 
+            this.pic_Large.BackColor = System.Drawing.Color.RosyBrown;
+            this.pic_Large.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Large.Location = new System.Drawing.Point(314, 104);
+            this.pic_Large.Name = "pic_Large";
+            this.pic_Large.Size = new System.Drawing.Size(300, 300);
+            this.pic_Large.TabIndex = 23;
+            this.pic_Large.TabStop = false;
+            this.pic_Large.Visible = false;
+            // 
+            // pic_Small
+            // 
+            this.pic_Small.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pic_Small.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_Small.Location = new System.Drawing.Point(359, 149);
+            this.pic_Small.Name = "pic_Small";
+            this.pic_Small.Size = new System.Drawing.Size(210, 210);
+            this.pic_Small.TabIndex = 24;
+            this.pic_Small.TabStop = false;
+            this.pic_Small.Visible = false;
             // 
             // Pizza_ChooseSize
             // 
@@ -38,14 +207,44 @@
             this.BackgroundImage = global::PizzaCraft.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(814, 511);
+            this.Controls.Add(this.pic_Small);
+            this.Controls.Add(this.pic_Middle);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lbl_Small);
+            this.Controls.Add(this.lbl_Middle);
+            this.Controls.Add(this.lbl_Large);
+            this.Controls.Add(this.lbl_Price);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_Large);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pizza_ChooseSize";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pizza Craft - Choose Appropriate Size";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Middle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Large)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Small)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Large;
+        private System.Windows.Forms.Label lbl_Middle;
+        private System.Windows.Forms.Label lbl_Small;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pic_Middle;
+        private System.Windows.Forms.PictureBox pic_Large;
+        private System.Windows.Forms.PictureBox pic_Small;
     }
 }
